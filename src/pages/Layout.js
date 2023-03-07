bdb06458b08f03e89c1ed36825b52a9df11794d7
Kevin Christopher <devstar0703@gmail.com>
2023-03-07 19:15:39 +0900

    update blog edit part

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -4,22 +4,28 @@ import { Routes , Route } from "react-router-dom";
 
 import { connect } from 'react-redux' ;
 
-import Header from "src/components/Layouts/Header";
+import Auth from "./Auth";
+import Dashboard from "./Dashboard";
+import Public from "./Public";
+
+import { MainLayout } from "./styled/Layout.styled";
+
+import 'react-circular-progressbar/dist/styles.css';
+import 'react-phone-number-input/style.css';
 
 const Layout = (props) => {
-  
-
     return (
-        <>
-            <Header />
+        <MainLayout>
             <Routes>
-                {/* <Route path="/" element={< Landing />} /> */}
+                <Route path='/auth/*' element={<Auth />} /> 
+                <Route path='/dashboard/*' element={<Dashboard />} />
+                <Route path="/*" element={<Public />} />
                 {/* <Route element={<ProtectedRoute />}>
                     <Route path='/solstice/*' element={<Solstice />} />
                 </Route>  */}
                 {/* <Route path="/*" element={<NotFound />}/> */}
             </Routes>
-        </>
+        </MainLayout>
     );
 }
 const mapStateToProps = state => ({
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,12 @@
 import * as React from 'react' ;
 
 import { Provider } from 'react-redux';
-import store from '../redux' ;
-// Router
+import store from 'src/redux' ;
+
 import { BrowserRouter , Routes , Route } from 'react-router-dom';
+
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import darkTheme from 'src/utils/theme/darkTheme';
 
 const Layout = React.lazy(() => import('./Layout')) ;
 
@@ -12,14 +15,14 @@ const Main = () => {
         <BrowserRouter>
             {/* <LanguageProvider> */}
                 <Provider store={store}>
-                    {/* <ThemeProvider theme={theme}> */}
-                        {/* <CssBaseline /> */}
+                    <ThemeProvider theme={darkTheme}>
+                        <CssBaseline />
                         <React.Suspense fallback={<React.Fragment />} >
                             <Routes>
                                 <Route path="*" element={<Layout />} />
                             </Routes>
                         </React.Suspense>
-                    {/* </ThemeProvider> */}
+                    </ThemeProvider>
                 </Provider>
             {/* </LanguageProvider> */}
         </BrowserRouter>
